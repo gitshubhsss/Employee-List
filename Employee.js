399691834f4bed1e4c702bee4ca5dde91cc43070
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-12 08:33:19 +0530

    multiple parameters has passed with one line that is ...add_employee

--- Employee.js
+++ Employee.js
@@ -6,8 +6,8 @@ let des = prompt("Enter your designation");
 let experience = prompt(`how many year of experience you have as an ${des}`);
 let c_name = prompt("Enter the componey name");
 
-let add_employee = (e_name, age, des, experience, c_name) => {
-  employee_list.push({ e_name, age, des, experience, c_name });
+let add_employee = (...add_employee) => {
+  employee_list.push({ ...add_employee });
   alert("Employee details added successfully");
 };
 
@@ -22,5 +22,13 @@ let show_details = () => {
   });
 };
 
-add_employee(e_name,age,des,experience,c_name);
+add_employee(e_name, age, des, experience, c_name);
 show_details();
+
+/* Employee details
+Employee.js:17 Employee 1
+Name -Shubham Ranjane
+Age - 21
+Designation -software Devoloper
+Experience -0-1
+Componey name - self-employeed */
